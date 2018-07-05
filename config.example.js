8f97baf94c585737d947430132743c2620e8bb8b
rainkolwa <pluvo.lee@gmail.com>
2018-07-05 12:03:52 +0800

    add today on history

---
+++ config.example.js
@@ -0,0 +1,3 @@
+module.exports = {
+  toh_key: ""
+};
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const _ = require("lodash");
+const axios = require("axios");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const { toh_key } = require("./config.js");
 
 const app = express();
 app.use(express.json());
@@ -9,7 +11,7 @@ app.use(express.json());
 const adapter = new FileSync("db.json");
 const db = low(adapter);
 
-//
+// one一个
 app.get("/api/luck", (req, res) => {
   const id = _.random(1, 360);
   const post = db
@@ -17,6 +19,15 @@ app.get("/api/luck", (req, res) => {
     .find({ id })
     .value();
   res.send(post);
+});
+
+// 历史上的今天
+app.get("/api/toh", async (req, res) => {
+  const { month, day } = req.query || {};
+  const { data } = await axios.get(
+    `http://v.juhe.cn/todayOnhistory/queryEvent.php?key=${toh_key}&date=${month}/${day}`
+  );
+  res.send(data);
 });
 
 const port = process.env.PORT || 3000;
